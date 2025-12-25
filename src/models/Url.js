1fc55d6beec05ef867e35da01f6bc56cd77545f0
MLILA05 <dmlemegency1234@gmail.com>
2025-12-24 21:00:00 -0800

    Add Url model with originalUrl and shortCode fields

---
+++ src/models/Url.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortCode: {
+    type: String,
+    unique: true,
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Url", urlSchema);
